7dcbfdac2df2d1fab7d9098085b84378c07a654e
Xelit3 <xavi_abelaira@outlook.com>
2022-09-15 09:48:26 +0200

    Implemented benchmarks for async await task comparision

---
+++ Xelit3.Benchmarks/Xelit3.Benchmarks/AsyncAwaitBenchmarks.cs
@@ -0,0 +1,29 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace Xelit3.Benchmarks;
+
+[MemoryDiagnoser(false)]
+public class AsyncAwaitBenchmarks
+{
+    [Benchmark]
+    public async Task<bool> ReturnFromTaskMethod()
+    {
+        return await ReturnTaskMethod();
+    }
+
+    [Benchmark]
+    public async Task<bool> ReturnFromAwaitMethod()
+    {
+        return await ReturnAwaitMethod();
+    }
+
+    public Task<bool> ReturnTaskMethod()
+    {
+        return Task.FromResult(true);
+    }
+    public async Task<bool> ReturnAwaitMethod()
+    {
+        return await Task.FromResult(true);
+    }
+
+}
---
+++ Xelit3.Benchmarks/Xelit3.Benchmarks/Program.cs
@@ -0,0 +1,6 @@
+﻿using BenchmarkDotNet.Running;
+using Xelit3.Benchmarks;
+
+var summary = BenchmarkRunner.Run<AsyncAwaitBenchmarks>();
+
+Console.ReadKey();
