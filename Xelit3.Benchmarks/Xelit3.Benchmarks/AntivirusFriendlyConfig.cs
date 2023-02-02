6e162c85b470f22039605bef9178f0813ae383d6
Xelit3 <xavi_abelaira@outlook.com>
2023-02-02 14:01:55 +0100

    Added ManualConfig for Windows Defender

---
+++ Xelit3.Benchmarks/Xelit3.Benchmarks/AntivirusFriendlyConfig.cs
@@ -0,0 +1,14 @@
+﻿using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Toolchains.InProcess.NoEmit;
+
+namespace Xelit3.Benchmarks
+{
+    public class AntivirusFriendlyConfig : ManualConfig
+    {
+        public AntivirusFriendlyConfig()
+        {
+            AddJob(Job.MediumRun.WithToolchain(InProcessNoEmitToolchain.Instance));
+        }
+    }
+}
