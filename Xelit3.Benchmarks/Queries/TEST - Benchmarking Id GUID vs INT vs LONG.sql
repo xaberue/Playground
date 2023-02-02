a47b17b33842858abcb1771964e85bc550963ce7
Xelit3 <xavi_abelaira@outlook.com>
2023-02-02 14:11:47 +0100

    Added test query for SqlServerEfCoreIdFetchBenchmarksHelper

---
+++ Xelit3.Benchmarks/Queries/TEST - Benchmarking Id GUID vs INT vs LONG.sql
@@ -0,0 +1,22 @@
+----THIS QUERY HAS BEEN CREATED ONLY FOR TESTING PURPOSES HELPING THE SqlServerEfCoreIdFetchBenchmarksHelper c# dotnet tests
+
+--SELECT * FROM [Test].[dbo].[Countries_Guid]
+--SELECT * FROM [Test].[dbo].[Countries_Int]
+--SELECT * FROM [Test].[dbo].[Countries_Long]
+
+
+SELECT TOP 1000 * FROM [Test].[dbo].[Persons_Guid]
+SELECT TOP 1000 * FROM [Test].[dbo].[Persons_Int]
+SELECT TOP 1000 * FROM [Test].[dbo].[Persons_Long]
+
+
+
+
+--DELETE FROM [Test].[dbo].[Countries_Guid]
+--DELETE FROM [Test].[dbo].[Countries_Int]
+--DELETE FROM [Test].[dbo].[Countries_Long]
+
+
+--DELETE FROM [Test].[dbo].[Persons_Guid]
+--DELETE FROM [Test].[dbo].[Persons_Int]
+--DELETE FROM [Test].[dbo].[Persons_Long]
