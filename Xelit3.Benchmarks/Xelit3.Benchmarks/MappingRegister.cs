c066950e01b0bb68d5a5f359119e7399830324c2
Xelit3 <xavi_abelaira@outlook.com>
2023-02-14 14:45:21 +0100

    Added MappingRegister : ICodeGenerationRegister

---
+++ Xelit3.Benchmarks/Xelit3.Benchmarks/MappingRegister.cs
@@ -0,0 +1,23 @@
+﻿using Mapster;
+using Xelit3.Tests.Model;
+
+namespace Xelit3.Benchmarks;
+
+internal class MappingRegister : ICodeGenerationRegister
+{
+    public void Register(CodeGenerationConfig config)
+    {
+        config.AdaptTo("[name]Dto").ForType<PersonDefault>();
+        config.AdaptTo("[name]Dto").ForType<AddressDefault>();
+        config.AdaptTo("[name]Dto").ForType<CityDefault>();
+        config.AdaptTo("[name]Dto").ForType<CountryDefault>();
+        config.AdaptTo("[name]Dto").ForType<PostDefault>();
+
+        config.GenerateMapper("[name]Mapper")
+            .ForType<PersonDefault>()
+            .ForType<AddressDefault>()
+            .ForType<CityDefault>()
+            .ForType<CountryDefault>()
+            .ForType<PostDefault>();
+    }
+}
