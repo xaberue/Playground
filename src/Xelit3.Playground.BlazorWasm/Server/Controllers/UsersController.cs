21c9c5f53fb7f40951cac60d6c7bbd8ae2127376
Xelit3 <xavi_abelaira@outlook.com>
2023-05-09 14:17:54 +0200

    changed API paths with 'api'

--- src/Xelit3.Playground.BlazorWasm/Server/Controllers/UsersController.cs
+++ src/Xelit3.Playground.BlazorWasm/Server/Controllers/UsersController.cs
@@ -6,7 +6,7 @@ using Xelit3.Playground.SqlServer;
 namespace Xelit3.Playground.BlazorWasm.Server.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     public class UsersController : ControllerBase
     {
         
