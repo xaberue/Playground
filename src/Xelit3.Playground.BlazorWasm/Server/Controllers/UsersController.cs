7ce54f1c4b88058bd9a2229b22fac5cf719cee10
Xelit3 <xavi_abelaira@outlook.com>
2023-05-16 07:29:32 +0200

    Users pagination implementation in Blazor WASM client

--- src/Xelit3.Playground.BlazorWasm/Server/Controllers/UsersController.cs
+++ src/Xelit3.Playground.BlazorWasm/Server/Controllers/UsersController.cs
@@ -4,55 +4,56 @@ using Xelit3.Playground.BlazorWasm.Shared;
 using Xelit3.Playground.SqlServer;
 using Xelit3.Tests.Model.Models;
 
-namespace Xelit3.Playground.BlazorWasm.Server.Controllers
+namespace Xelit3.Playground.BlazorWasm.Server.Controllers;
+
+
+[ApiController]
+[Route("api/[controller]")]
+public class UsersController : ControllerBase
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class UsersController : ControllerBase
+
+    private readonly EFTestDataContext _dbContext;
+
+
+    public UsersController(EFTestDataContext eFTestDataContext)
     {
-
-        private readonly EFTestDataContext _dbContext;
-
-
-        public UsersController(EFTestDataContext eFTestDataContext)
-        {
-            _dbContext = eFTestDataContext;
-        }
+        _dbContext = eFTestDataContext;
+    }
 
 
-        [HttpGet]
-        public IActionResult GetAll()
-        {
-            var users = _dbContext.Persons_Guid
-                .Include(x => x.Origin)
-                .ToList();
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+        var users = _dbContext.Persons_Guid
+            .Include(x => x.Origin)
+            .ToList();
 
-            return Ok(users
-                .Select(x => new UserDto(x.Id, $"{x.Name} {x.Surname}", x.Origin?.Name ?? "Unknown", x.BirthDate)));
-        }
+        return Ok(users
+            .Select(x => new UserDto(x.Id, $"{x.Name} {x.Surname}", x.Origin?.Name ?? "Unknown", x.BirthDate)));
+    }
 
-        [HttpGet("paginated")]
-        public IActionResult GetAll(int page, int size)
-        {
-            var users = _dbContext.Persons_Guid
-                .Include(x => x.Origin)
-                .Skip(size * page).Take(size)
-                .ToList();
+    [HttpGet("paginated")]
+    public IActionResult GetAll(int page, int size)
+    {
+        var users = _dbContext.Persons_Guid
+            .Include(x => x.Origin)
+            .OrderBy(x => x.Name)
+            .Skip(size * page).Take(size)
+            .ToList();
 
-            return Ok(users
-                .Select(x => new UserDto(x.Id, $"{x.Name} {x.Surname}", x.Origin?.Name ?? "Unknown", x.BirthDate)));
-        }
+        return Ok(users
+            .Select(x => new UserDto(x.Id, $"{x.Name} {x.Surname}", x.Origin?.Name ?? "Unknown", x.BirthDate)));
+    }
 
 
-        [HttpPost]
-        public IActionResult Save(UserCreationDto userCreationDto)
-        {
-            var user = new Person<Guid> { OriginId = userCreationDto.OriginId, Name = userCreationDto.Name, Surname = userCreationDto.Surname, BirthDate = userCreationDto.BirthDate };
+    [HttpPost]
+    public IActionResult Save(UserCreationDto userCreationDto)
+    {
+        var user = new Person<Guid> { OriginId = userCreationDto.OriginId, Name = userCreationDto.Name, Surname = userCreationDto.Surname, BirthDate = userCreationDto.BirthDate };
 
-            _dbContext.Persons_Guid.Add(user);
-            _dbContext.SaveChanges();
+        _dbContext.Persons_Guid.Add(user);
+        _dbContext.SaveChanges();
 
-            return CreatedAtAction("Save", new UserDto(user.Id, $"{user.Name} {user.Surname}", user.Origin?.Name ?? "Unknown", user.BirthDate));
-        }
+        return CreatedAtAction("Save", new UserDto(user.Id, $"{user.Name} {user.Surname}", user.Origin?.Name ?? "Unknown", user.BirthDate));
     }
 }
