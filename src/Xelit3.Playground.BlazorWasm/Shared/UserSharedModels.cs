1a13c977b29c3c79839e386381dedeef5fa729a7
Xelit3 <xavi_abelaira@outlook.com>
2023-10-13 09:15:47 +0200

    Fixing ctor issue on UserCreationDto

--- src/Xelit3.Playground.BlazorWasm/Shared/UserSharedModels.cs
+++ src/Xelit3.Playground.BlazorWasm/Shared/UserSharedModels.cs
@@ -21,4 +21,14 @@ public record UserCreationDto
     public string Surname { get; set; } = null!;
     [Required]
     public DateTime BirthDate { get; set; }
+
+    public UserCreationDto(){ }
+
+    public UserCreationDto(Guid originId, string name, string surname, DateTime birthDate)
+    {
+        OriginId = originId;
+        Name = name;
+        Surname = surname;
+        BirthDate = birthDate;
+    }
 }
