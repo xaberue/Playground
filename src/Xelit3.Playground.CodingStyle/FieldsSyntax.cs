d59562ceb767196e28eb8afc2a426898ec3bd038
Xelit3 <xavi_abelaira@outlook.com>
2023-11-16 15:09:44 +0100

    private field sylly discussion

---
+++ src/Xelit3.Playground.CodingStyle/FieldsSyntax.cs
@@ -0,0 +1,37 @@
+﻿namespace Xelit3.Playground.CodingStyle;
+
+
+public class User { }
+
+public record UserDto { }
+
+public record UserCreationParams { }
+
+
+public interface IUserService
+{
+
+}
+
+public class UserController
+{
+
+    private readonly IUserService _userService;
+
+    private readonly IUserService userService;
+
+    private object? UserSomething;
+
+
+    public UserController(IUserService userService)
+    {
+        this.userService = userService;
+    }
+
+
+    public void Create(UserCreationParams userCreationParams)
+    {
+        //Implementation goes here
+    }
+
+}
