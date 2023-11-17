e1972640b546743ab2f987ca340461d6c4fd6177
Xelit3 <xavi_abelaira@outlook.com>
2023-11-17 09:30:33 +0100

    UserCreation workflow implemented

--- src/Xelit3.Playground.Users.Shared/Models/UserrDto.cs
+++ src/Xelit3.Playground.Users.Shared/Models/UserrDto.cs
@@ -1,4 +1,5 @@
-﻿using Xelit3.Playground.Users.Shared.Enums;
+﻿using System.ComponentModel.DataAnnotations;
+using Xelit3.Playground.Users.Shared.Enums;
 
 namespace Xelit3.Playground.Users.Shared.Models;
 
@@ -6,7 +7,20 @@ namespace Xelit3.Playground.Users.Shared.Models;
 public record UserDto(int Id, string Name, string Surname, string Email, DateTime BirthDate, RoleType Role);
 
 
-public record CreateUserDto(string Name, string Surname, string Email, string Password, DateTime BirthDate);
+public record CreateUserDto 
+{
+    [Required]
+    public string Name { get; set; } = null!;
+    [Required]
+    public string Surname{ get; set; } = null!;
+    [Required]
+    [EmailAddress]
+    public string Email{ get; set; } = null!;
+    [Required]
+    public string Password { get; set; } = null!;
+    [Required]
+    public DateTime BirthDate { get; set; }
+}
 
 
 public record UpdateUserDto(int Id, string Name, string Surname, string Email, DateTime BirthDate);
