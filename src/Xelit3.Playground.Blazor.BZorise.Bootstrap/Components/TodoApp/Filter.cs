8cef34b2d87797f7827ba6769893d7762fa1a68e
Xelit3 <xavi_abelaira@outlook.com>
2023-12-07 07:54:14 +0100

    Added Blazorise template using Bootstrap5

---
+++ src/Xelit3.Playground.Blazor.BZorise.Bootstrap/Components/TodoApp/Filter.cs
@@ -0,0 +1,8 @@
+﻿namespace Xelit3.Playground.Blazor.BZorise.Bootstrap.Components.TodoApp;
+
+public enum Filter
+{
+    All,
+    Active,
+    Completed,
+}
---
+++ src/Xelit3.Playground.Blazor.BZorise.Bootstrap/Components/TodoApp/Todo.cs
@@ -0,0 +1,8 @@
+﻿namespace Xelit3.Playground.Blazor.BZorise.Bootstrap.Components.TodoApp;
+
+public class Todo
+{
+    public bool Completed { get; set; }
+
+    public string? Description { get; set; }
+}
