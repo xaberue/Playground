32a195b09ae6788906d9332f4ad53cd830bc2112
Xelit3 <xavi_abelaira@outlook.com>
2023-12-19 09:56:43 +0100

    Finnished main gRPC service

--- src/Xelit3.Playground.API.Grpc/Models/ToDoItem.cs
+++ src/Xelit3.Playground.API.Grpc/Models/ToDoItem.cs
@@ -2,8 +2,32 @@
 
 public class ToDoItem
 {
-    public int Id { get; set; }
-    public string Title { get; set; } = null!;
-    public string Description { get; set; } = null!;
-    public ToDoItemStatus Status { get; set; }
+    public int Id { get; init; }
+    public string Title { get; private set; } = null!;
+    public string Description { get; private set; } = null!;
+    public ToDoItemStatus Status { get; init; }
+
+
+
+    public ToDoItem(string title, string description)
+    {
+        Title = title;
+        Description = description;
+        Status = ToDoItemStatus.New;
+    }
+
+    public ToDoItem(int id, string title, string description, ToDoItemStatus status)
+    {
+        Id = id;
+        Title = title;
+        Description = description;
+        Status = status;
+    }
+
+
+    internal void Update(string title, string description)
+    {
+        Title = title;
+        Description = description;
+    }
 }
---
+++ src/Xelit3.Playground.API.Grpc/Services/TodoService.cs
@@ -0,0 +1,81 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using Microsoft.EntityFrameworkCore;
+using Xelit3.Playground.API.Grpc.Data;
+using Xelit3.Playground.API.Grpc.Models;
+
+namespace Xelit3.Playground.API.Grpc.Services;
+
+public class TodoService : ToDo.ToDoBase
+{
+
+    private readonly ILogger<TodoService> _logger;
+    private readonly ToDoDataContext _context;
+
+
+    public TodoService(ILogger<TodoService> logger, ToDoDataContext context)
+    {
+        _logger = logger;
+        _context = context;
+    }
+
+
+    public override async Task<GetMultipleToDoResponse> GetAllToDo(GetAllToDoRequest request, ServerCallContext context)
+    {
+        var items = await _context.ToDoItems.ToListAsync();
+        var response = new GetMultipleToDoResponse();
+
+        foreach (var item in items)
+        {
+            response.ToDos.Add(new GetSingleToDoResponse
+            {
+                Id = item.Id,
+                Title = item.Title,
+                Description = item.Description,
+                ToDoStatus = item.Status.ToString()
+            });
+        }
+
+        return response;
+    }
+
+    public override async Task<GetSingleToDoResponse> GetSingleToDo(GetSingleToDoRequest request, ServerCallContext context)
+    {
+        var item = await _context.ToDoItems.FindAsync(request.Id) ?? throw new RpcException(new Status(StatusCode.NotFound, $"ToDoItem with id {request.Id} not found"));
+
+        return new GetSingleToDoResponse { Id = item.Id, Title = item.Title, Description = item.Description, ToDoStatus = item.Status.ToString() };
+    }
+
+    public override async Task<CreateToDoResponse> CreateToDo(CreateToDoRequest request, ServerCallContext context)
+    {
+        if(string.IsNullOrWhiteSpace(request.Title) || string.IsNullOrWhiteSpace(request.Description))
+            throw new RpcException(new Status(StatusCode.InvalidArgument, "Title and Description are required"));
+
+        var todoItem = new ToDoItem(request.Title, request.Description);
+
+        await _context.ToDoItems.AddAsync(todoItem);
+        await _context.SaveChangesAsync();
+
+        return await Task.FromResult(new CreateToDoResponse{ Id = todoItem.Id });
+    }
+
+    public override async Task<UpdateToDoResponse> UpdateToDo(UpdateToDoRequest request, ServerCallContext context)
+    {
+        var todoItem = await _context.ToDoItems.FindAsync(request.Id) ?? throw new RpcException(new Status(StatusCode.NotFound, $"ToDoItem with id {request.Id} not found"));
+
+        todoItem.Update(request.Title, request.Description);        
+        await _context.SaveChangesAsync();
+
+        return await Task.FromResult(new UpdateToDoResponse { Id = todoItem.Id });
+    }
+
+    public override async Task<DeleteToDoResponse> DeleteToDo(DeleteToDoRequest request, ServerCallContext context)
+    {
+        var todoItem = await _context.ToDoItems.FindAsync(request.Id) ?? throw new RpcException(new Status(StatusCode.NotFound, $"ToDoItem with id {request.Id} not found"));
+
+        _context.ToDoItems.Remove(todoItem);
+        await _context.SaveChangesAsync();
+
+        return await Task.FromResult(new DeleteToDoResponse { Id = request.Id });
+    }
+}
