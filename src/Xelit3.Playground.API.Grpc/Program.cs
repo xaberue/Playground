339b28e423e95cd03c4948e0c7c5f7781c23ba6b
Xelit3 <xavi_abelaira@outlook.com>
2023-12-20 11:57:24 +0100

    Added Swagger to gRPC sample API

--- src/Xelit3.Playground.API.Grpc/Program.cs
+++ src/Xelit3.Playground.API.Grpc/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using Xelit3.Playground.API.Grpc.Data;
 using Xelit3.Playground.API.Grpc.Services;
 
@@ -11,9 +12,20 @@ builder.Services.AddDbContext<ToDoDataContext>(opt => {
 
 builder.Services.AddGrpc().AddJsonTranscoding();
 builder.Services.AddGrpcReflection();
+builder.Services.AddGrpcSwagger();
+builder.Services.AddSwaggerGen(x => 
+{
+    x.SwaggerDoc("v1", new OpenApiInfo { Title = "ToDo API", Version = "v1" });
+});
 
 
 var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI(x =>
+{
+    x.SwaggerEndpoint("/swagger/v1/swagger.json", "ToDo API v1");
+});
 
 app.MapGrpcService<TodoService>();
 app.MapGrpcReflectionService();
