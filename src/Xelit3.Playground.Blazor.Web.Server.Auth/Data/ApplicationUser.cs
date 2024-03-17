2c6199929c273590dba6929e6e59b9919ede8c59
Xelit3 <xavi_abelaira@outlook.com>
2024-03-17 11:05:16 +0100

    Blazor web Server interactive render Auth project template

---
+++ src/Xelit3.Playground.Blazor.Web.Server.Auth/Data/ApplicationUser.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace Xelit3.Playground.Blazor.Web.Server.Auth.Data
+{
+    // Add profile data for application users by adding properties to the ApplicationUser class
+    public class ApplicationUser : IdentityUser
+    {
+    }
+
+}
