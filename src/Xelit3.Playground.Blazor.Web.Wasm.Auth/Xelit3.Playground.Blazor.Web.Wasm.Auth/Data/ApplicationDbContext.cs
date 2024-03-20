0576b09a0c3cdb5e99a18a848b550782c8ccde3e
Xelit3 <xavi_abelaira@outlook.com>
2024-03-20 18:28:46 +0100

    Removed template-only things Blazor project
    Removed template-only things Blazor project

--- src/Xelit3.Playground.Blazor.Web.Wasm.Auth/Xelit3.Playground.Blazor.Web.Wasm.Auth/Data/ApplicationDbContext.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
-
-namespace Xelit3.Playground.Blazor.Web.Wasm.Auth.Data
-{
-    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
-    {
-    }
-}
