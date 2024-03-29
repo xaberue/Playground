1250440fc567f202977fd54310c5b0a258d8274e
Xelit3 <xavi_abelaira@outlook.com>
2024-03-29 19:42:46 +0100

    Added model validation binding

--- src/Xelit3.Playground.Users.WebUI.Tests/UserCreationComponentTests.cs
+++ src/Xelit3.Playground.Users.WebUI.Tests/UserCreationComponentTests.cs
@@ -51,16 +51,21 @@ public class UserCreationComponentTests : TestContext
         var userService = Substitute.For<IUserService>();
         userService.CreateAsync(default!).ReturnsForAnyArgs(Task.CompletedTask);
         Services.AddScoped<IUserService>(x => userService);
+        var expectedName = "Name";
+        var expectedSurname = "Surname";
+        var expectedEmail = "test@email.com";
+        var expectedPassword = "Pa$$w0rd.343_";
+        var expectedBirthdate = DateTime.Now;
 
         //Act
         var nav = Services.GetRequiredService<FakeNavigationManager>();
         var cut = RenderComponent<UserCreation>();
 
-        cut.Find("#form-input-name").Change("Test");
-        cut.Find("#form-input-surname").Change("Surname");
-        cut.Find("#form-input-email").Change("test@email.com");
-        cut.Find("#form-input-password").Change("Test.1234!");
-        cut.Find("#form-input-birthdate").Change(DateTime.Now.ToString("yyyy-MM-dd"));
+        cut.Find("#form-input-name").Change(expectedName);
+        cut.Find("#form-input-surname").Change(expectedSurname);
+        cut.Find("#form-input-email").Change(expectedEmail);
+        cut.Find("#form-input-password").Change(expectedPassword);
+        cut.Find("#form-input-birthdate").Change(expectedBirthdate.ToString("yyyy-MM-dd"));
 
         var submitButton = cut.Find("button[type=submit]");
         submitButton.Click();
@@ -68,6 +73,11 @@ public class UserCreationComponentTests : TestContext
         //Assert
         cut.Find("h3").MarkupMatches("<h3>New User</h3>");
         Assert.DoesNotContain("validation-message", cut.Markup);
+        Assert.Equal(expectedName, cut.Instance.Model.Name);
+        Assert.Equal(expectedSurname, cut.Instance.Model.Surname);
+        Assert.Equal(expectedEmail, cut.Instance.Model.Email);
+        Assert.Equal(expectedPassword, cut.Instance.Model.Password);
+        Assert.Equal(expectedBirthdate.Date, cut.Instance.Model.BirthDate.Date);
         Assert.Equal("http://localhost/users", nav.Uri);
     }
 
