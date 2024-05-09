5f22af6d68a09cf89aa39755a0bab1dfd858c0a7
Xelit3 <xavi_abelaira@outlook.com>
2024-05-09 11:17:53 +0200

    Empty MVP project for SignalR

---
+++ dotnet/Xelit3.Playground.SignalR/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Xelit3.Playground.SignalR.Models;
+
+namespace Xelit3.Playground.SignalR.Controllers;
+
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ dotnet/Xelit3.Playground.SignalR/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Xelit3.Playground.SignalR.Models;
+
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
