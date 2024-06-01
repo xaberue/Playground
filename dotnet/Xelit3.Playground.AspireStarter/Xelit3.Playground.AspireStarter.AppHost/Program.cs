1ad0deefbfc4fb1214a99e041233d72bfe068195
Xelit3 <xavi_abelaira@outlook.com>
2024-06-01 09:50:43 +0200

    Upgrade Aspire Starter project to GA release 8.0.1

--- dotnet/Xelit3.Playground.AspireStarter/Xelit3.Playground.AspireStarter.AppHost/Program.cs
+++ dotnet/Xelit3.Playground.AspireStarter/Xelit3.Playground.AspireStarter.AppHost/Program.cs
@@ -5,6 +5,7 @@ var cache = builder.AddRedis("cache");
 var apiService = builder.AddProject<Projects.Xelit3_Playground_AspireStarter_ApiService>("apiservice");
 
 builder.AddProject<Projects.Xelit3_Playground_AspireStarter_Web>("webfrontend")
+    .WithExternalHttpEndpoints()
     .WithReference(cache)
     .WithReference(apiService);
 
