db22a6ee598fe76135fc9f5a09fac0194d334905
Xelit3 <xavi_abelaira@outlook.com>
2024-06-25 09:58:46 +0200

    projection calculation and storing in event sourcing sample

--- dotnet/Xelit3.Playground.EventSourcing/BankDatabase.cs
+++ dotnet/Xelit3.Playground.EventSourcing/BankDatabase.cs
@@ -4,30 +4,34 @@ using Xelit3.Playground.EventSourcing;
 public class BankDatabase
 {
 
-    private readonly Dictionary<Guid, SortedList<DateTime, Event>> _bankAccounts = new();
+    private readonly Dictionary<Guid, SortedList<DateTime, Event>> _bankAccountsEvents = new();
+    private readonly Dictionary<Guid, BankAccount> _bankAccounts = new();
 
     public void Append(Event @event)
     {
-        var stream = _bankAccounts.GetValueOrDefault(@event.StreamId, null);
+        var stream = _bankAccountsEvents.GetValueOrDefault(@event.StreamId, null);
 
         if (stream is null)
         {
-            _bankAccounts[@event.StreamId] = new SortedList<DateTime, Event>();
+            _bankAccountsEvents[@event.StreamId] = new SortedList<DateTime, Event>();
         }
 
-        _bankAccounts[@event.StreamId].Add(@event.CreationDate, @event);
+        _bankAccountsEvents[@event.StreamId].Add(@event.CreationDate, @event);
+
+        //This can be different, the simple approach is calculating the entire projection each time, but we can recover the last one and just apply the latest event on top of that
+        _bankAccounts[@event.StreamId] = GetAccount(@event.StreamId);
     }
 
 
     public BankAccount? GetAccount(Guid accountId)
     {
-        if (!_bankAccounts.ContainsKey(accountId))
+        if (!_bankAccountsEvents.ContainsKey(accountId))
         {
             return null;
         }
 
         var account = new BankAccount();
-        var events = _bankAccounts[accountId];
+        var events = _bankAccountsEvents[accountId];
 
         foreach (var @event in events)
         {
@@ -37,4 +41,8 @@ public class BankDatabase
         return account;
     }
 
+    public BankAccount? GetAccountView(Guid accountId)
+    {
+        return _bankAccounts.GetValueOrDefault(accountId);
+    }
 }
