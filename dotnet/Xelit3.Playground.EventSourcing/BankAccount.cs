9e0de68c7e42c8c7c92247cc73814437bfdfd6e0
Xelit3 <xavi_abelaira@outlook.com>
2024-06-25 09:38:58 +0200

    Basic eventsourcing example added

---
+++ dotnet/Xelit3.Playground.EventSourcing/BankAccount.cs
@@ -0,0 +1,35 @@
+﻿namespace Xelit3.Playground.EventSourcing;
+
+public class BankAccount
+{
+    public Guid AccountId { get; private set; }
+    public string Number { get; private set; } = string.Empty;
+    public decimal Balance { get; private set; } = 0;
+
+
+    public void Apply(Event @event)
+    {
+        switch (@event)
+        {
+            case BankAccountCreated bankAccountCreated:
+                Apply(bankAccountCreated);
+                break;
+
+            case BankAccountBalanceChanged bankAccountBalanceChanged:
+                Apply(bankAccountBalanceChanged);
+                break;
+        }
+    }
+
+    private void Apply(BankAccountCreated bankAccountCreated)
+    {
+        AccountId = bankAccountCreated.StreamId;
+        Number = bankAccountCreated.Number;
+        Balance = 0;
+    }
+
+    private void Apply(BankAccountBalanceChanged bankAccountBalanceChanged)
+    {
+        Balance += bankAccountBalanceChanged.Quantity;
+    }
+}
---
+++ dotnet/Xelit3.Playground.EventSourcing/BankDatabase.cs
@@ -0,0 +1,40 @@
+﻿
+using Xelit3.Playground.EventSourcing;
+
+public class BankDatabase
+{
+
+    private readonly Dictionary<Guid, SortedList<DateTime, Event>> _bankAccounts = new();
+
+    public void Append(Event @event)
+    {
+        var stream = _bankAccounts.GetValueOrDefault(@event.StreamId, null);
+
+        if (stream is null)
+        {
+            _bankAccounts[@event.StreamId] = new SortedList<DateTime, Event>();
+        }
+
+        _bankAccounts[@event.StreamId].Add(@event.CreationDate, @event);
+    }
+
+
+    public BankAccount? GetAccount(Guid accountId)
+    {
+        if (!_bankAccounts.ContainsKey(accountId))
+        {
+            return null;
+        }
+
+        var account = new BankAccount();
+        var events = _bankAccounts[accountId];
+
+        foreach (var @event in events)
+        {
+            account.Apply(@event.Value);
+        }
+
+        return account;
+    }
+
+}
