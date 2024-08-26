dfe3b884edb9ef09b5a2ac4a2d942c523fd8b5b6
Xelit3 <xavi_abelaira@outlook.com>
2024-08-26 17:21:22 +0200

    General WebAPI for videogames, to test Docker and multiple things

---
+++ dotnet/Xelit3.Playground.Videogames.WebApi/Program.cs
@@ -0,0 +1,80 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Xelit3.Playground.Videogames.WebApi.Models;
+using Xelit3.Playground.Videogames.WebApi.Dtos;
+using Xelit3.Playground.Videogames.WebApi.Infrastructure;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<VideogamesDbContext>(x => 
+{
+    x.UseSqlServer(builder.Configuration.GetConnectionString("SqlServerConnectionString"));
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+var group = app.MapGroup("api");
+
+group.MapGet("/all", (VideogamesDbContext dbContext) =>
+{
+    var all = dbContext.Videogames.AsAsyncEnumerable();
+
+    return Results.Ok(all);
+});
+
+group.MapGet("/{id}", async (int id, VideogamesDbContext dbContext) =>
+{
+    var entity = await dbContext.Videogames.FindAsync(id);
+
+    return entity is null ? Results.NotFound() : Results.Ok(entity);
+});
+
+group.MapPost("", async (VideogameCreationDto creationDto, VideogamesDbContext dbContext) => 
+{
+    var entity = new Videogame(creationDto.Name, creationDto.Publisher, creationDto.YearReleased, creationDto.Genre);
+
+    await dbContext.AddAsync(entity);
+    await dbContext.SaveChangesAsync();
+
+    return Results.Created($"/{entity.Id}", entity.ToDto());
+});
+
+group.MapPut("", async (VideogameEditionDto editionDto, VideogamesDbContext dbContext) =>
+{
+    var entity = await dbContext.Videogames.FindAsync(editionDto.Id);
+
+    if (entity is null)
+        return Results.NotFound();
+
+    entity.Update(editionDto.Name, editionDto.Publisher, editionDto.YearReleased, editionDto.Genre);
+
+    await dbContext.SaveChangesAsync();
+
+    return Results.Ok(entity.ToDto());
+});
+
+group.MapDelete("/{id}", async (int id, VideogamesDbContext dbContext) =>
+{
+    var entity = await dbContext.Videogames.FindAsync(id);
+
+    if (entity is null)
+        return Results.NotFound();
+
+    dbContext.Remove(entity);
+    await dbContext.SaveChangesAsync();
+
+    return Results.NoContent();
+});
+
+app.Run();
