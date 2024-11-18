5facc45f938e673d1aeb4d237faa6a84db020aa5
Xelit3 <xavi_abelaira@outlook.com>
2024-11-18 19:19:25 +0100

    rabbitMQ sample project added

---
+++ dotnet/Xelit3.Playground.RabbitMQ.Receiver/Program.cs
@@ -0,0 +1,40 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+ConnectionFactory factory = new();
+factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+factory.ClientProvidedName = "Rabbit Receiver App";
+
+var connection = await factory.CreateConnectionAsync();
+var channel = await connection.CreateChannelAsync();
+
+var exchangeName = "DemoExchange";
+var routingKey = "demo-routing-key";
+var queueName = "DemoQueue";
+
+await channel.ExchangeDeclareAsync(exchangeName, ExchangeType.Direct);
+await channel.QueueDeclareAsync(queueName, durable: false, exclusive: false, autoDelete: false);
+await channel.QueueBindAsync(queueName, exchangeName, routingKey);
+
+await channel.BasicQosAsync(prefetchSize: 0, prefetchCount: 1, global: false);
+
+var consumer = new AsyncEventingBasicConsumer(channel);
+
+consumer.ReceivedAsync += async (sender, args) =>
+{
+    var body = args.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+
+    Console.WriteLine($"Message received: {message}");
+
+    await channel.BasicAckAsync(args.DeliveryTag, multiple: false);
+};
+
+var consumerTag = await channel.BasicConsumeAsync(queueName, autoAck: false, consumer);
+
+Console.ReadLine();
+
+await channel.BasicCancelAsync(consumerTag);
+await channel.CloseAsync();
+await connection.CloseAsync();
---
+++ dotnet/Xelit3.Playground.RabbitMQ.Sender/Program.cs
@@ -0,0 +1,36 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+
+ConnectionFactory factory = new();
+factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+factory.ClientProvidedName = "Rabbit Sender App";
+
+var connection = await factory.CreateConnectionAsync();
+var channel = await connection.CreateChannelAsync();
+
+var exchangeName = "DemoExchange";
+var routingKey = "demo-routing-key";
+var queueName = "DemoQueue";
+
+await channel.ExchangeDeclareAsync(exchangeName, ExchangeType.Direct);
+await channel.QueueDeclareAsync(queueName, durable: false, exclusive: false, autoDelete: false);
+await channel.QueueBindAsync(queueName, exchangeName, routingKey);
+
+//byte[] messageBody = Encoding.UTF8.GetBytes("Hello World!");
+
+//await channel.BasicPublishAsync(exchangeName, routingKey, messageBody);
+
+for (var i = 1; i <= 1000; i++)
+{
+    Console.WriteLine($"Sending message: {i}");
+
+    var messageBody = Encoding.UTF8.GetBytes($"Hello World #{i}!");
+
+    await channel.BasicPublishAsync(exchangeName, routingKey, messageBody);
+
+    await Task.Delay(100);
+}
+
+await channel.CloseAsync();
+await connection.CloseAsync();
