08499ec435592b755234a53bf17021acb68e3d8e
Xelit3 <xavi_abelaira@outlook.com>
2024-12-30 18:28:21 +0100

    not working implementation from Andrew Lock

---
+++ dotnet/Xelit3.Playground.ResultPattern/Implementation.cs
@@ -0,0 +1,60 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Security.Claims;
+
+namespace Xelit3.Playground.ResultPattern;
+
+public class UserProvisioningService(CreateUserService createUserService)
+{
+    public Result<UserAccount> ProvisionUser(ExternalLoginInfo info)
+    {
+        // 1. Try to get the initial claim results
+        Result<Claim[]> claimsResult = GetClaimValues(info);
+
+        return claimsResult.Switch(
+            onSuccess: claims => // if the claims we retrieved successfully, run this function
+            {
+                // 2. Try to validate the claims
+                Result<Claim[]> validatedClaimsResult = ValidateClaims(claims);
+                return validatedClaimsResult.Switch(
+                    onSuccess: validatedClaims =>  // validation was successful
+                    {
+                        // 3. Try to extract the tenant ID
+                        Result<Guid> tenantIdResult = GetTenantId(claims);
+                        return tenantIdResult.Switch(
+                            onSuccess: tenantId => // extracted successfully
+                            {
+                                // 4. Create the ProvisionUserRequest object
+                                Result<ProvisionUserRequest> createRequestResult =
+                                    CreateProvisionUserRequest(tenantId, validatedClaims);
+                                return createRequestResult.Switch<Result<UserAccount>>(
+                                    onSuccess: createRequest => // created the request successfully
+                                    {
+                                        // 5. Try to create the account, and return the Result<UserAccount>
+                                        return createUserService.GetOrCreateAccount(createRequest);
+                                    },
+                                    onFailure: ex => Result<UserAccount>.Fail(ex)); // Step 4 failed, return the error 
+                            },
+                            onFailure: ex => Result<UserAccount>.Fail(ex)); // Step 3 failed, return the error 
+
+                    },
+                    onFailure: ex => Result<UserAccount>.Fail(ex)); // Step 2 failed, return the error 
+            },
+            onFailure: ex => Result<UserAccount>.Fail(ex)); // Step 1 failed, return the error 
+    }
+
+    // Helper methods (just stubs)
+    private Claim[]? GetClaimValues(ExternalLoginInfo loginInfo) => null;
+    private Claim[]? ValidateClaims(Claim[]? claims) => claims;
+    private Guid GetTenantId(Claim[]? claims) => Guid.NewGuid();
+    private ProvisionUserRequest CreateProvisionUserRequest(Guid employerId, Claim[]? claims) => new();
+}
+
+// Helper types/services
+public record ExternalLoginInfo;
+public record ProvisionUserRequest;
+public record UserAccount;
+
+public class CreateUserService
+{
+    public UserAccount GetOrCreateAccount(ProvisionUserRequest request) => new();
+}
---
+++ dotnet/Xelit3.Playground.ResultPattern/ResultPattern.cs
@@ -0,0 +1,58 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Xelit3.Playground.ResultPattern;
+
+public class Result<T>
+{
+    // We don't expose these publicly any more
+    private readonly T? _value;
+    private readonly Exception? _error;
+
+    // Same constructors
+    private Result(T value)
+    {
+        IsSuccess = true;
+        _value = value;
+        _error = null;
+    }
+
+    private Result(Exception error)
+    {
+        IsSuccess = false;
+        _value = default;
+        _error = error;
+    }
+
+    [MemberNotNullWhen(true, nameof(_value))]
+    [MemberNotNullWhen(false, nameof(_error))]
+    private bool IsSuccess { get; }
+
+    // This Method takes two Func<T>, one for the success case and one for the error case
+    public Result<TReturn> Switch<TReturn>(
+        Func<T, TReturn> onSuccess,
+        Func<Exception, Exception> onFailure)
+    {
+        if (IsSuccess)
+        {
+            // If this result has a value, run the success method,
+            // which returns a different value, and then we create a
+            // Result<TReturn> from it (implicitly)
+            var result = onSuccess(_value);
+            return result;
+        }
+        else
+        {
+            {
+                // If this result is an error, run the error method
+                // to allow the user to manipulate/inspect the error.
+                // We then create a new Result<TReturn> result object
+                // from the error it returns
+                var err = onFailure(_error);
+                return Result<TReturn>.Fail(err);
+            }
+        }
+
+    public static Result<T> Success(T value) => new(value);
+    public static Result<T> Fail(Exception error) => new(error);
+    public static implicit operator Result<T>(T value) => Success(value);
+}
