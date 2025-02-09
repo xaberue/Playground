f3f586290fdd7a78e38560230b0ed7de3326b937
xaberue <xavier.aberue@outlook.es>
2025-02-09 19:24:41 +0100

    General NuGet upgrade and few breaking changes adaptations

--- dotnet/Xelit3.Playground.BlazorHybrid/App.xaml.cs
+++ dotnet/Xelit3.Playground.BlazorHybrid/App.xaml.cs
@@ -1,12 +1,14 @@
-﻿namespace Xelit3.Playground.BlazorHybrid
-{
-    public partial class App : Application
-    {
-        public App()
-        {
-            InitializeComponent();
+﻿namespace Xelit3.Playground.BlazorHybrid;
 
-            MainPage = new MainPage();
-        }
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+    }
+
+    protected override Window CreateWindow(IActivationState? activationState)
+    {
+        return new Window(new MainPage()) { Title = "Xelit3.Playground.BlazorHybrid" };
     }
 }
