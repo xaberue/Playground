e3617efa396a87327aaa4a6b5d568276207710a8
xaberue <xavier.aberue@outlook.es>
2025-04-28 23:28:08 +0200

    How to configure globally Serialization

--- dotnet/Xelit3.Playground.API.Minimal.Rest/Program.cs
+++ dotnet/Xelit3.Playground.API.Minimal.Rest/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Http.Json;
 using Microsoft.EntityFrameworkCore;
 using Xelit3.Playground.API.Minimal.Rest;
 using Xelit3.Playground.API.Shared.Data;
@@ -12,6 +13,15 @@ builder.Services.AddDbContext<ToDoDataContext>(opt =>
     opt.UseSqlite("Data Source=ToDoDatabase.db");
 });
 
+/*How to configure globally Serialization, different options*/
+//builder.Services.Configure<JsonOptions>(options =>
+//{
+//    options.SerializerOptions.PropertyNamingPolicy = null;
+//    options.SerializerOptions.WriteIndented = true;
+//    options.SerializerOptions.PropertyNameCaseInsensitive = true;
+
+//    //options.SerializerOptions.PropertyNamingPolicy = System.Text.Json.JsonNamingPolicy.CamelCase;
+//});
 
 
 var app = builder.Build();
