ff2c6ce9cfa04d0cc067e869d1f9c1a6deb9f691
xaberue <xavier.aberue@outlook.es>
2025-06-09 18:37:17 +0200

    using ConfigureAppConfiguration in WebApplicationFactory with xUnit

--- dotnet/Xelit3.Playground.BlazorWasm.IntegrationTests/Configuration/CustomWebApplicationFactory.cs
+++ dotnet/Xelit3.Playground.BlazorWasm.IntegrationTests/Configuration/CustomWebApplicationFactory.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Xelit3.Playground.SqlServer;
 
@@ -9,6 +10,12 @@ public class CustomWebApplicationFactory<TProgram> : WebApplicationFactory<TProg
 {
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
+        // NOTE: This is a better way to configure the application for testing purposes.
+        //builder.ConfigureAppConfiguration((context, config) =>
+        //{
+        //    config.AddJsonFile("appsettings.Test.json", optional: false, reloadOnChange: true);
+        //});
+
         builder.ConfigureServices(services =>
         {
             var dbContextDescriptor = services.SingleOrDefault(d => d.ServiceType == typeof(EFTestDataContext));
