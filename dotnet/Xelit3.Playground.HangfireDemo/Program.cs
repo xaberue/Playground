6aec2da83e83d2c822cc6f5b0cb240388d44db9e
xaberue <xavier.aberue@outlook.es>
2026-02-05 14:51:19 +0100

    Basic Hangfire setup with inmemory storage

---
+++ dotnet/Xelit3.Playground.HangfireDemo/Program.cs
@@ -0,0 +1,35 @@
+using Hangfire;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddOpenApi();
+
+builder.Services.AddHangfire(conf => 
+    conf
+        .UseSimpleAssemblyNameTypeSerializer()
+        .UseRecommendedSerializerSettings()
+        .UseInMemoryStorage()
+);
+
+builder.Services.AddHangfireServer(x => 
+x.SchedulePollingInterval = TimeSpan.FromSeconds(1));  //Don't do this in PROD, we are requesting each second knowing the jobs to be executed. By default is 15 seconds.
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+
+app.MapGet("/job", (IBackgroundJobClient jobClient) =>
+{
+    jobClient.Enqueue(() => Console.WriteLine("Hello from Hangfire!"));
+
+    return Results.Ok();
+});
+
+app.Run();
+
